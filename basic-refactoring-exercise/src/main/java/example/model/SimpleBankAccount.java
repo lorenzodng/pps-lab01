2a2a564c41e10865711d579661599b1e3967e60f
Lorenzo D'Angelo <lorenzodangelo00@gmail.com>
2025-02-28 14:41:37 +0100

    withdraw method change

--- basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccount.java
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccount.java
@@ -34,7 +34,7 @@ public class SimpleBankAccount implements BankAccount {
     @Override
     public void withdraw(final int userID, final double amount) {
         if (checkUser(userID) && isWithdrawAllowed(amount)) {
-            this.balance -= amount + 1;
+            this.balance = this.balance - amount - 1;
         }
     }
 
