150f9da9a5afaab5d70d677ba8526f2482ddad4f
Lorenzo D'Angelo <lorenzodangelo00@gmail.com>
2025-03-23 16:00:45 +0100

    changes

--- tdd-exercise/src/main/java/tdd/SimpleCircularQueue.java
+++ tdd-exercise/src/main/java/tdd/SimpleCircularQueue.java
@@ -5,60 +5,60 @@ import java.util.Collections;
 
 public class SimpleCircularQueue implements CircularQueue {
 
-    private final int CAPACITY;
-    private int firstPosition= 0;
-    private final ArrayList<Integer> QUEUE;
+    private final int capacity;
+    private int firstPosition;
+    private final ArrayList<Integer> queue;
 
-    public SimpleCircularQueue(int CAPACITY){
-        this.CAPACITY= CAPACITY;
-        QUEUE= new ArrayList<>(CAPACITY);
+    public SimpleCircularQueue(int capacity){
+        this.capacity= capacity;
+        queue= new ArrayList<>(capacity);
     }
 
     @Override
     public void add(int value) {
-        if(QUEUE.size()==CAPACITY) {
-            QUEUE.set(firstPosition, value);
-            firstPosition= (firstPosition+1) % CAPACITY;
+        if(queue.size()==capacity) {
+            queue.set(firstPosition, value);
+            firstPosition= (firstPosition + 1) % capacity;
         }else {
-            QUEUE.add(value);
+            queue.add(value);
         }
     }
 
     @Override
     public int remove() {
         if(!isEmpty()) {
-            return QUEUE.remove(firstPosition);
+            return queue.remove(firstPosition);
         }else throw new IllegalStateException();
     }
 
     @Override
     public int peek() {
         if(!isEmpty()) {
-            return QUEUE.get(firstPosition);
+            return queue.get(firstPosition);
         }else throw new IllegalStateException();
     }
 
     @Override
     public int getMin() {
         if(!isEmpty()) {
-            return Collections.min(QUEUE);
+            return Collections.min(queue);
         }else throw new IllegalStateException();
     }
 
     @Override
     public int getMax() {
         if(!isEmpty()) {
-            return Collections.max(QUEUE);
+            return Collections.max(queue);
         }else throw new IllegalStateException();
     }
 
     @Override
     public int size() {
-        return QUEUE.size();
+        return queue.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return QUEUE.isEmpty();
+        return queue.isEmpty();
     }
 }
